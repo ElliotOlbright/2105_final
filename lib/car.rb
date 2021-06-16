b4080c606afc2c5a0efb9a599b68aadd53733415
Elliot Olbright <elliotolbright@gmail.com>
2021-06-16 09:05:56 -0600

    add readable attributes test

---
+++ lib/car.rb
@@ -0,0 +1,4 @@
+class Car 
+  def initialize(data_hash)
+  end 
+end 
--- spec/car_spec.rb
+++ spec/car_spec.rb
@@ -7,11 +7,15 @@ require './lib/train'
 RSpec.describe Car do
   describe 'Object Creation' do
     before :each do
-      car1 = Car.new({type: 'Mail', weight: 5})
+      @car1 = Car.new({type: 'Mail', weight: 5})
     end
 
     it 'exists do' do
-      expect(car1).to be_a(Car)
+      expect(@car1).to be_a(Car)
     end
+
+    it 'has readable attributes' do
+      expect(@car1.type).to eq('Mail')
+    end 
   end
 end
