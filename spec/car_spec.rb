df26fcfc55ec55baed20ba087a69c6508fdb0827
Elliot Olbright <elliotolbright@gmail.com>
2021-06-16 09:04:30 -0600

    create car initialize test

---
+++ spec/car_spec.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+require './lib/car'
+require './lib/train'
+
+
+
+RSpec.describe Car do
+  describe 'Object Creation' do
+    before :each do
+      car1 = Car.new({type: 'Mail', weight: 5})
+    end
+
+    it 'exists do' do
+      expect(car1).to be_a(Car)
+    end
+  end
+end
